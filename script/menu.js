219cb306649b9922095b3f122654d126752506b2
Joel Canté <97580350+JoelCante@users.noreply.github.com>
2023-12-23 00:55:00 -0600

    Add files via upload
    Primera versión de mi sitio web

---
+++ script/menu.js
@@ -0,0 +1,34 @@
+
+window.addEventListener("scroll", function(){
+  var menu = document.getElementById('menu-flotante');
+  menu.classList.toggle("abajo",window.scrollY>0);
+})
+
+//------------------------------
+var body = $('body');
+var boton = document.getElementById('boton-menu');
+var botonoculto = document.getElementById('boton-menu-oculto');
+
+function abrir() {
+    $("#menu").slideToggle();
+    body.addClass('sin-scroll');
+    botonoculto.style.display = "block";
+    boton.style.display ="none";
+}
+
+
+function  cerrar(){
+  $("#menu").slideToggle();
+    body.removeClass('sin-scroll');
+    botonoculto.style.display = "none";
+    boton.style.display ="block";
+} 
+
+//------------------------------
+  $(document).ready(function(){
+    $('#subir').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 1000);
+    });
+  });
